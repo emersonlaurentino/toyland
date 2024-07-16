98f260c408debbd7f87c6c5477b6f4ca697edd47
Emerson Laurentino <emerson@laurentino.co>
2024-07-16 19:03:59 -0300

    feat: Update tab name from "explore" to "marketplace"
    Update the tab name from "explore" to "marketplace" in the TabLayout component. This change improves the clarity and consistency of the tab names in the application.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -53,9 +53,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="marketplace"
         options={{
-          title: "Explorar",
+          title: "Marketplace",
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon
               name={focused ? "compass" : "compass-outline"}
